59dccc137ce13dd7c28e5c77f11a9e141d16cca5
Guo Wei Jian <weijian.guo@gameloft.com>
2016-04-12 01:00:12 +0800

    NewsDetailActivity now could show simple news html page

--- app/src/main/java/org/weijian/mydriversrss/NewsDetailActivity.kt
+++ app/src/main/java/org/weijian/mydriversrss/NewsDetailActivity.kt
@@ -1,9 +1,13 @@
 package org.weijian.mydriversrss
 
+import android.graphics.drawable.Drawable
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.webkit.WebView
+import android.text.Html
+import android.text.Spanned
+import android.widget.TextView
+import org.jsoup.Jsoup
 import java.net.URL
 
 class NewsDetailActivity : AppCompatActivity() {
@@ -12,20 +16,42 @@ class NewsDetailActivity : AppCompatActivity() {
         const val NEWS_URL = "NEWS_URL"
     }
 
-    lateinit var webView: WebView
+    lateinit var mNewsInfo: TextView
+    lateinit var retrieveTask: RetrieveNewsTask
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_detail)
-        webView = findViewById(R.id.webview) as WebView
-        val url = URL(intent.getStringExtra(NEWS_URL))
-        webView.loadUrl(url.toString())
+        mNewsInfo = findViewById(R.id.news_info) as TextView
+        retrieveTask = RetrieveNewsTask()
+
+        val url = intent.getStringExtra(NEWS_URL)
+        retrieveTask.execute(url)
 
     }
 
-    inner class RetrieveNews : AsyncTask<URL, Void, String>() {
-        override fun doInBackground(vararg params: URL?): String? {
-            throw UnsupportedOperationException()
+    class ImageGetter : Html.ImageGetter {
+        override fun getDrawable(source: String?): Drawable? {
+            var drawable: Drawable? = null
+            if (source != null) {
+                val url = URL(source)
+                drawable = Drawable.createFromStream(url.openStream(), "")
+            }
+            drawable?.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+            return drawable
+        }
+    }
+
+    inner class RetrieveNewsTask : AsyncTask<String, Void, Spanned>() {
+        override fun doInBackground(vararg params: String?): Spanned {
+            var doc = Jsoup.connect(params[0]).get()
+            val newsInfo = doc.getElementsByClass("news_info").select("p")
+            val html = Html.fromHtml(newsInfo.html(), ImageGetter(), Html.TagHandler { opening, tag, output, xmlReader -> })
+            return html
         }
 
+        override fun onPostExecute(result: Spanned) {
+            mNewsInfo.text = result
+        }
     }
 }
